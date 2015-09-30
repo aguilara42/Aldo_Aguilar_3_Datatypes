b212be49245986b19138a441b059053473bd5d38
D22 <aguilara42@ninja23.d22.lan>
2015-09-30 06:14:50 -0500

    New

---
+++ src/datatypes.java
@@ -0,0 +1,56 @@
+
+public class datatypes {
+	public static void main(String[] args){
+		int i = 0;
+		float a = 0.01f;
+		double b = 0.01;
+		short c = 123;
+		char d1 = ';';
+		char d2 = 'a';
+		char d3 = 42;
+		char d4 = 145; 
+		String e = "Words";
+		String e1 = "Aldo is the best";
+		boolean f = true;
+		boolean g = i % 2 == 0; 
+		
+		/*math
+		 *+ addition 
+		 *- subtraction 
+		 * * Multiplies numbers
+		 * / Division sign
+		 * % Takes the remainder
+		 *
+		 * 
+		 */
+		
+		System.out.println(i + " This is an integer");
+		System.out.println(a + " a float");
+		System.out.println(b + " a double");
+		System.out.println(c + " a short");
+		System.out.println(d1 + " a char");
+		System.out.println(d2 + " a char");
+		System.out.println(d3 + " a char");
+		System.out.println(d4 + " a char");
+		System.out.println(e + " a string");
+		System.out.println(f + " a boolean");
+		System.out.println(g + " a boolean");
+
+		
+//		for(i = -123; i<200; i ++){
+//			System.out.println((char) i);
+//			System.out.println(g);
+//			
+			// char shows a number value for characters in the font
+		System.out.println(i + a + " This is an integer plus a float");
+		System.out.println(a + b + " a float plus a double");
+		System.out.println(b + c + " a double plus a short");
+		System.out.println(c + " a short");
+		
+		}
+		
+	
+	
+	
+	}
+
